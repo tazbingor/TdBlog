c48596eaffa9af3984345f7c79da238105802035
tazbingor <852353298@qq.com>
2018-03-01 07:55:23 +0800

    views method

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse('欢迎访问我的博客首页')
+    file_path = 'blog/index.html'
+    return render(request, file_path, context={
+        'title': '博客首页',
+        'welcome': '欢迎访问我的博客!'
+    })
