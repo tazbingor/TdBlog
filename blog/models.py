e76e019e2dd40302cccdba7e48ddd907cbb74e5a
tazbingor <852353298@qq.com>
2018-03-01 06:55:53 +0800

    db models

--- blog/models.py
+++ blog/models.py
@@ -13,15 +13,16 @@ class Tag(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=80)  # 文章标题
+
     body = models.TextField()  # 正文,使用TextField()储存大段的文本
 
     created_time = models.DateTimeField()  # 文章修改前后的时间
     modified_time = models.DateTimeField()
 
-    excerpt = models.CharField(max_length=200, blank=True)  # CharField不允许出现空值,所以必须声明blank=True
+    excerpt = models.CharField(max_length=200, blank=True)  # 文章摘要,CharField不允许出现空值,所以必须声明blank=True
 
-    category = models.ForeignKey(Category)
-    tags = models.ManyToManyField(Tag, blank=True)
+    category = models.ForeignKey(Category)  # 分类
+    tags = models.ManyToManyField(Tag, blank=True)  # 标签
 
     # 文章作者,User由django.contrib.auth.models导入
     author = models.ForeignKey(User)
