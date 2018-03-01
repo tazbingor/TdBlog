553bb676ee7e688f1dfb51789b85d65450e7272c
tazbingor <852353298@qq.com>
2018-03-01 08:40:26 +0800

    views method

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 
 # Create your views here.
 
 def index(request):
     file_path = 'blog/index.html'
+    post_list = Post.objects.all().order_by('-created_time')
     return render(request, file_path, context={
-        'title': '博客首页',
-        'welcome': '欢迎访问我的博客!'
+        'post_list': post_list
     })
