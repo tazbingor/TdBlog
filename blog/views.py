34b089400d4663461fd528412d5436827e78bc0b
tazbingor <852353298@qq.com>
2018-03-01 11:24:15 +0800

    views detail

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 
@@ -10,4 +10,12 @@ def index(request):
     post_list = Post.objects.all().order_by('-created_time')
     return render(request, file_path, context={
         'post_list': post_list
+    })
+
+
+def detail(request, pk):
+    file_path = 'blog/detail.html'
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, file_path, context={
+        'post': post
     })
