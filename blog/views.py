e60ca38dc48b91a2b2b8bce22ee51d7ae2821af3
tazbingor <852353298@qq.com>
2018-03-03 12:54:18 +0800

    侧边栏

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 import markdown
+from .models import Post, Category
 
 
 # Create your views here.
@@ -27,3 +28,15 @@ def detail(request, pk):
                                   ])
 
     return render(request, file_path, context={'post': post})
+
+
+def archives(request, year, mouth):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=mouth).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+
+def category(request, pk):
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
