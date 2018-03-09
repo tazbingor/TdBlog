c2510c719eccf48d1ec03bc9b14e9a932126e36c
tazbingor <852353298@qq.com>
2018-03-09 16:10:56 +0800

    评论

--- comments/models.py
+++ comments/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 from django.db import models
 from django.utils.six import python_2_unicode_compatible
+from django.urls import utils
 
 
 @python_2_unicode_compatible
