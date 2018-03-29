afb5b78a3655910cd94fc3c824a2c975b00e1f49
tazbingor <852353298@qq.com>
2018-03-29 23:17:33 +0800

    标签云

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -5,7 +5,7 @@
 from django import template
 from django.db.models.aggregates import Count
 
-from ..models import Post, Category
+from ..models import Post, Category, Tag
 
 register = template.Library()
 
@@ -36,3 +36,12 @@ def get_categories():
     :return:
     '''
     return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
+
+@register.simple_tag()
+def get_tags():
+    '''
+    标签云
+    :return:
+    '''
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
