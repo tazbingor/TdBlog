69b9551edcc951ae7b18e199d728b3b6f563b17c
tazbingor <852353298@qq.com>
2018-03-29 23:03:33 +0800

    文章数量

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -5,7 +5,7 @@
 from django import template
 from django.db.models.aggregates import Count
 
-from blog.models import Post, Category
+from ..models import Post, Category
 
 register = template.Library()
 
@@ -17,7 +17,8 @@ def get_recent_posts(num=5):
     :param num:
     :return:
     '''
-    return Post.objects.all().order_by('-created_time')[:num]
+    # return Post.objects.all().order_by('-created_time')[:num]
+    return Post.objects.all()[:num]
 
 
 @register.simple_tag
