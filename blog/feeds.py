e63c7a9df4d36e06c624c1138bc7b7acf2c4f1b6
tazbingor <852353298@qq.com>
2018-04-06 13:30:21 +0800

    rss

---
+++ blog/feeds.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from django.contrib.syndication.views import Feed
+from .models import Post
+
+
+class AllPostsRssFeed(Feed):
+    title = "测试项目"
+    link = "/"
+    description = "测试文章"
+
+    def items(self):
+        return Post.objects.all()
+
+    def item_title(self, item):
+        return '[%s] %s' % (item.category, item.title)
+
+    def item_description(self, item):
+        return item.body
